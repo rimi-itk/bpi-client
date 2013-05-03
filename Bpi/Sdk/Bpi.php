67050688b6071bfe8499a94466b6cc8172c6f8d8
George Mihailov <gm@inlead.dk>
2013-05-03 17:01:51 +0300

    case 2693 - Handle HTTP errors.

--- Bpi/Sdk/Bpi.php
+++ Bpi/Sdk/Bpi.php
@@ -1,5 +1,4 @@
 <?php
-namespace Bpi\Sdk;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
@@ -12,7 +11,7 @@ class Bpi
     public function __construct($endpoint, $agency_id, $api_key, $secret_key)
     {
         $this->client = new \Goutte\Client();
-        $this->authorization = new Authorization($agency_id, $api_key, $secret_key);
+        $this->authorization = new \Bpi\Sdk\Authorization($agency_id, $api_key, $secret_key);
         $this->endpoint = $this->createDocument();
         $this->endpoint->loadEndpoint($endpoint);
     }
@@ -24,12 +23,14 @@ class Bpi
      */
     protected function createDocument()
     {
-        return new Document($this->client, $this->authorization);
+        return new \Bpi\Sdk\Document($this->client, $this->authorization);
     }
 
     /**
+     * Get list of node based on some conditions
      *
-     * @param array $queries
+     * @param array $queries available keys are: amount, offset, filter, sort
+     *   filter and sort requires nested arrays
      * @return \Bpi\Sdk\NodeList
      */
     public function searchNodes(array $queries = array())
@@ -44,6 +45,29 @@ class Bpi
             ->send($nodes, $queries);
 
         $nodes->reduceItemsByAttr('type', 'entity');
-        return new NodeList($nodes);
+        return new \Bpi\Sdk\NodeList($nodes);
+    }
+
+    /**
+     * Push new node to BPI
+     *
+     * @param array $data
+     * @throws \InvalidArgumentException
+     * @return \Bpi\Sdk\Item\Node
+     */
+    public function push(array $data)
+    {
+        $node = $this->createDocument();
+        $nodes = clone $this->endpoint;
+        $nodes->firstItem('name', 'node')
+            ->template('push')
+            ->eachField(function ($field) use ($data) {
+                if (!isset($data[(string)$field]))
+                    throw new \InvalidArgumentException(sprintf('Field [%s] is required', (string) $field));
+
+                $field->setValue($data[(string) $field]);
+            })->post($node);
+
+        return new \Bpi\Sdk\Item\Node($node);
     }
 }
