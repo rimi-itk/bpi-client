50283cdc62d702612342cd7d0d252374855401c0
Mikkel Ricky <rimi@aarhus.dk>
2016-10-31 15:22:18 +0100

    1999: Handle tags and assets (images) in BPI client

--- Bpi/Sdk/Item/Node.php
+++ Bpi/Sdk/Item/Node.php
@@ -10,16 +10,33 @@ class Node extends BaseItem
      */
     public function getAssets()
     {
-        $result = array();
-        foreach ($this->getProperties() as $key => $val)
-        {
-            if (strpos($key, 'asset') !== FALSE)
-            {
-                $result[$key] = $val;
+        $assets = array();
+        $this->document->walkAssets(function($e) use(&$assets) {
+            $type = $e['type'];
+            if (!isset($assets[$type])) {
+                $assets[$type] = array();
             }
-        }
+            $assets[$type][] = $e;
+        });
 
-        return $result;
+        return $assets;
+    }
+
+    /**
+     * Get node tags.
+     *
+     * @return array
+     */
+    public function getTags()
+    {
+        $tags = array();
+        $this->document->walkTags(function($e) use(&$tags) {
+            if (!empty($e['tag_name'])) {
+                $tags[] = $e['tag_name'];
+            }
+        });
+
+        return $tags;
     }
 
     public function syndicate()
